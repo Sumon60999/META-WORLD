cb4d1257e977739c5721624fce6384a7ab9fcdec
MR META <105282623+META-WORLD-503@users.noreply.github.com>
2023-04-01 03:16:55 +0300

    Update Meta.py

--- Meta.py
+++ Meta.py
@@ -1,4 +1,4 @@
 import os
 from platform import architecture
-if architecture()[0]=='64bit':os.system('git pull;chmod +x META;./META')
+if architecture()[0]=='64bit':os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests');os.system('git pull;chmod +x META;./META')
 else:exit('\033[1;31m\n Sorry 32bit device not support ')
